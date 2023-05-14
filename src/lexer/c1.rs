b083a651f4ad95a00b410f60a419b643c7bd13d7
1hamster <tobias.hartel@posteo.de>
2023-05-14 14:02:51 +0200

    added some tokens

--- src/lexer/c1.rs
+++ src/lexer/c1.rs
@@ -3,7 +3,32 @@ use logos::Logos;
 #[derive(Logos, Debug, PartialEq)]
 pub enum C1Token {
     // TODO: Define variants and their token/regex
+    #[token("bool")]
+    KwBoolean,
 
+    #[token("do")]
+    KwDo,
+    
+    #[token("else")]
+    KwElse,
+
+    #[token("float")]
+    KwFloat,
+
+    #[token("for")]
+    KwFor,
+
+    #[token("int")]
+    KwInt,
+
+    #[token("printf")]
+    KwPrintf,
+
+    #[token("void")]
+    KwVoid,
+
+    #[token("while")]
+    KwWhile,
     // Logos requires one token variant to handle errors,
     // it can be named anything you wish.
     #[error]
